d001acdf9c81d88a456bb39a8adf72c1471ff8f6
Azis Ramadhan <git@azrd.dev>
2025-09-15 13:55:22 +0700

    fix: write subset target to file instead of passing directly & some refactors

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,59 @@
+import { rm } from "node:fs/promises";
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import z from "zod";
 
 const TEMP_DIR = ".tmp";
+const app = new Hono().post(
+    "/v1/subset",
+    zValidator("form", z.object({
+        subset_text: z.string().min(1).max(15_000),
+        font_file: z
+            .file()
+            .mime(["font/otf", "font/ttf", "font/woff", "font/woff2"])
+            .max(20_000_000),
+        output: z.enum(["ttf", "woff2"]),
+    })),
+    cors({ origin: "*" }),
+    async (c) => {
+        const { font_file, subset_text, output } = c.req.valid("form");
+        const req_dir = `${TEMP_DIR}/${Bun.randomUUIDv7()}`;
 
-export const app = new Hono().post(
-	"/v1/subset",
-	zValidator(
-		"form",
-		z.object({
-			subset_text: z.string().min(1),
-			font_file: z
-				.file()
-				.mime(["font/otf", "font/ttf", "font/woff", "font/woff2"])
-				.max(10_000_000),
-			output: z.enum(["ttf", "woff2"]),
-		}),
-	),
-	cors({ origin: "*" }),
-	async (c) => {
-		const { font_file, subset_text, output } = c.req.valid("form");
-		await Bun.write(`${TEMP_DIR}/font.ttf`, font_file);
-		await Bun.$`hb-subset --face-loader=ft \
-            ${TEMP_DIR}/font.ttf \
-            --text=${subset_text} \
-            -o ${TEMP_DIR}/output.ttf`;
+        await Promise.all([
+            Bun.write(`${req_dir}/font.ttf`, font_file),
+            Bun.write(`${req_dir}/target.txt`, subset_text)
+        ])
+        await Bun.$`hb-subset \
+            --face-loader=ft \
+            --font-file="${req_dir}/font.ttf" \
+            --text-file=${req_dir}/target.txt \
+            -o ${req_dir}/output.ttf`;
 
-		let file = Bun.file(`${TEMP_DIR}/output.ttf`);
-		if (output === "woff2") {
-			await Bun.$`woff2_compress ${TEMP_DIR}/output.ttf`.quiet();
-			file = Bun.file(`${TEMP_DIR}/output.woff2`);
-			return c.body(await file.arrayBuffer(), 200, {
-				"Content-Type": "font/woff2",
-			});
-		}
-		return c.body(await file.arrayBuffer(), 200, {
-			"Content-Type": "font/ttf",
-		});
-	},
+        let out_path = `${req_dir}/output.ttf`;
+        let content_type = "font/ttf";
+
+        if (output === "woff2") {
+            await Bun.$`woff2_compress ${req_dir}/output.ttf`.quiet();
+            out_path = `${req_dir}/output.woff2`;
+            content_type = "font/woff2";
+        }
+
+        const buffer = await Bun.file(out_path).arrayBuffer();
+        await rm(req_dir, { recursive: true })
+        return c.body(buffer, 200, { "Content-Type": content_type });
+    },
 );
 
-if (import.meta.main) {
-	const server = Bun.serve({
-		port: 4321,
-		fetch: app.fetch,
-	});
+const server = Bun.serve({
+    fetch: app.fetch,
+    port: 4321,
+});
 
-	["SIGINT", "SIGTERM"].map((signal) =>
-		process.on(signal, async () => {
-			await server.stop();
-			console.log("Shutting down.");
-			process.exit(0);
-		}),
-	);
-}
+["SIGINT", "SIGTERM"].map((sig) =>
+    process.on(sig, async () => {
+        await server.stop();
+        console.log("Shutting down.");
+        process.exit(0);
+    }),
+);
